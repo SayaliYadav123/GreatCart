2749ac749b7460619f56e92f6fee94adcbe2320f
SayaliYadav123 <sayaliyadav2122@gmail.com>
2026-03-04 13:52:12 +0530

    secret key

--- GreatCart/settings.py
+++ GreatCart/settings.py
@@ -158,9 +158,8 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER='sayaliyadav2122@gmail.com'
-
-EMAIL_HOST_PASSWORD='ojrbmjusaxrianal'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS=True
 import os
 
