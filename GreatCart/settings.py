00603c4fcc124436e4fabe12a3122c0d7e5189ad
SayaliYadav123 <sayaliyadav2122@gmail.com>
2026-03-05 17:21:45 +0530

    deployment setup

--- GreatCart/settings.py
+++ GreatCart/settings.py
@@ -35,9 +35,9 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 CSRF_TRUSTED_ORIGINS = [
     "https://*.ngrok-free.dev",
 ]
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ".ngrok-free.dev",]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ".ngrok-free.dev",]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'GreatCart.urls'
 
 TEMPLATES = [
@@ -136,10 +136,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT= BASE_DIR/'static'
+# STATICFILES_DIRS = [
+#     'GreatCart/static',
+# ]
 STATIC_URL = '/static/'
-STATIC_ROOT= BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
-    'GreatCart/static',
+    BASE_DIR / 'GreatCart/static',
 ]
 
 # Default primary key field type
